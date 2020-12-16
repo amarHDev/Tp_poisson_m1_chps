8eb76965a2ec465b4694edcfe97a45954ce34603
AmarHDev <henni.amar24@gmail.com>
2020-12-16 21:24:06 +0100

    Implementation et Remplissage de la fonction set_GB_operator_rowMajor_poisson1D

--- src/tp2_poisson1D_direct.c
+++ src/tp2_poisson1D_direct.c
@@ -35,7 +35,7 @@ int main(int argc,char *argv[])
   set_grid_points_1D(X, &la);
   set_dense_RHS_DBC_1D(RHS,&la,&T0,&T1);
   set_analytical_solution_DBC_1D(EX_SOL, X, &la, &T0, &T1);
-  
+
   write_vec(RHS, &la, "RHS.dat");
   write_vec(EX_SOL, &la, "EX_SOL.dat");
   write_vec(X, &la, "X_grid.dat");
@@ -57,18 +57,18 @@ int main(int argc,char *argv[])
   if (row == 1){ // LAPACK_ROW_MAJOR
     set_GB_operator_rowMajor_poisson1D(AB, &lab, &la);
     //write_GB_operator_rowMajor_poisson1D(AB, &lab, &la, "AB_row.dat");
-    
+
     info = LAPACKE_dgbsv(LAPACK_ROW_MAJOR,la, kl, ku, NRHS, AB, la, ipiv, RHS, NRHS);
-  
-  } 
+
+  }
   else { // LAPACK_COL_MAJOR
     set_GB_operator_colMajor_poisson1D(AB, &lab, &la, &kv);
     //write_GB_operator_colMajor_poisson1D(AB, &lab, &la, "AB_col.dat");
 
     info = LAPACKE_dgbsv(LAPACK_COL_MAJOR,la, kl, ku, NRHS, AB, lab, ipiv, RHS, la);
-  }    
+  }
 
-  
+
   printf("\n INFO DGBSV = %d\n",info);
 
   write_xy(RHS, X, &la, "SOL.dat");
@@ -80,7 +80,7 @@ int main(int argc,char *argv[])
   relres = cblas_ddot(la, EX_SOL, 1, EX_SOL,1);
   relres = sqrt(relres);
   relres = relres / temp;
-  
+
   printf("\nThe relative residual error is relres = %e\n",relres);
 
   free(RHS);
