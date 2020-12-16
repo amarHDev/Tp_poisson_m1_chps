573ab755ff37f675090f7a34472dd1b6623f29c7
AmarHDev <henni.amar24@gmail.com>
2020-12-16 21:21:06 +0100

    Implementation et Remplissage de la fonction set_GB_operator_rowMajor_poisson1D

--- src/lib_poisson1D.c
+++ src/lib_poisson1D.c
@@ -1,15 +1,31 @@
 /**********************************************/
 /* lib_poisson1D.c                            */
-/* Numerical library developed to solve 1D    */ 
+/* Numerical library developed to solve 1D    */
 /* Poisson problem (Heat equation)            */
 /**********************************************/
 #include "lib_poisson1D.h"
 
 void set_GB_operator_rowMajor_poisson1D(double* AB, int *lab, int *la){
+  printf("Row-Major-Executed\n");
 
-  //TODO
+  int jj, ii1, ii2, ii3, ii4;
+  for(jj=0;jj<(*la);jj++){
+    ii1=jj;
+    ii2=(*la)+jj;
+    ii3=2*(*la)+jj;
+    ii4=3*(*la)+jj;
+    AB[ii1]=0.0;
+    AB[ii2]=-1.0;
+    AB[ii3]=2.0;
+    AB[ii4]=-1.0;
+  }
+
+  AB[*la]=0.0;
+  AB[4*(*la)-1]=0.0;
 }
 void set_GB_operator_colMajor_poisson1D(double* AB, int *lab, int *la, int *kv){
+  printf("Col-Major-Executed\n");
+
   int ii, jj, kk;
   for (jj=0;jj<(*la);jj++){
     kk = jj*(*lab);
@@ -24,7 +40,7 @@ void set_GB_operator_colMajor_poisson1D(double* AB, int *lab, int *la, int *kv){
   }
   AB[0]=0.0;
   if (*kv == 1) {AB[1]=0;}
-  
+
   AB[(*lab)*(*la)-1]=0.0;
 }
 
@@ -52,7 +68,7 @@ void set_dense_RHS_DBC_1D(double* RHS, int* la, double* BC0, double* BC1){
   for (jj=1;jj<(*la)-1;jj++){
     RHS[jj]=0.0;
   }
-}  
+}
 
 void set_analytical_solution_DBC_1D(double* EX_SOL, double* X, int* la, double* BC0, double* BC1){
   int jj;
@@ -61,7 +77,7 @@ void set_analytical_solution_DBC_1D(double* EX_SOL, double* X, int* la, double* 
   for (jj=0;jj<(*la);jj++){
     EX_SOL[jj] = (*BC0) + X[jj]*DELTA_T;
   }
-}  
+}
 
 void set_grid_points_1D(double* x, int* la){
   int jj;
@@ -108,8 +124,8 @@ void write_vec(double* vec, int* la, char* filename){
   }
   else{
     perror(filename);
-  } 
-}  
+  }
+}
 
 void write_xy(double* vec, double* x, int* la, char* filename){
   int jj;
@@ -124,8 +140,8 @@ void write_xy(double* vec, double* x, int* la, char* filename){
   }
   else{
     perror(filename);
-  } 
-}  
+  }
+}
 
 void eig_poisson1D(double* eigval, int *la){
   int ii;
@@ -134,7 +150,7 @@ void eig_poisson1D(double* eigval, int *la){
     scal=(1.0*ii+1.0)*M_PI_2*(1.0/(*la+1));
     eigval[ii]=sin(scal);
     eigval[ii]=4*eigval[ii]*eigval[ii];
-  } 
+  }
 }
 
 double eigmax_poisson1D(int *la){
